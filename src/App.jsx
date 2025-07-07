7f9bac5f0b198af55203d3bf56f2737e73a1910d
Robel Alemayehu <robelalemayehu838@gmail.com>
2025-07-07 12:35:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./components/navBar.jsx"
+import Main from "./components/main.jsx"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
